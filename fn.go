f93e315ab90fe528a7feedb0ce8a56429630f905
tnthornton <2375126+tnthornton@users.noreply.github.com>
2025-07-01 14:08:34 -0700

    begin switching to langchaingo

--- fn.go
+++ fn.go
@@ -2,60 +2,414 @@ package main
 
 import (
 	"context"
+	"strings"
+	"text/template"
+
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+	"github.com/tmc/langchaingo/agents"
+	"github.com/tmc/langchaingo/chains"
+	openaillm "github.com/tmc/langchaingo/llms/openai"
+	"github.com/tmc/langchaingo/tools"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/types/known/structpb"
+	"sigs.k8s.io/yaml"
 
 	"github.com/crossplane/function-sdk-go/errors"
 	"github.com/crossplane/function-sdk-go/logging"
 	fnv1 "github.com/crossplane/function-sdk-go/proto/v1"
 	"github.com/crossplane/function-sdk-go/request"
+	"github.com/crossplane/function-sdk-go/resource"
 	"github.com/crossplane/function-sdk-go/response"
 
 	"github.com/upbound/function-openai/input/v1alpha1"
 )
 
-// Function returns whatever response you ask it to.
+const (
+	credName = "gpt"
+	credKey  = "OPENAI_API_KEY"
+)
+
+const system = `
+You are a Kubernetes templating tool designed to generate and update Kubernetes
+Resource Model (KRM) resources using Kubernetes server-side apply. Your task is
+to create, update, or delete YAML manifests based on the provided composite
+resource and any existing composed resources.
+`
+
+const prompt = `
+<instructions>
+Please follow these instructions carefully:
+
+1. Analyze the provided composite resource and any existing composed resources.
+
+2. Analyze the input to understand what composed resources you should create,
+   update, or delete. You may be asked to derive composed resources from the
+   composite resource, or from other composed resources.
+
+3. Generate a stream of YAML manifests based on your analysis in steps 1 and 2.
+   Each manifest should:
+   a. Be valid for Kubernetes server-side apply (fully specified intent).
+   b. Omit names and namespaces.
+   c. Include an annotation with the key "upbound.io/name". This annotation
+      must uniquely identify the manifest within the YAML stream. It must be
+      lowercase, hyphen separated, and less than 30 characters long. Prefer
+      to use the manifest's kind. If two or more manifests have the same
+      kind, look for something unique about the manifest and append that to
+      the kind. This annotation is used to match the manifests you return to
+      any manifests that were passed you inside the <composed> tag, so if
+      your intent is to update a manifest never change its "upbound.io/name"
+      annotation. This is critically important.
+   d. If it's necessary to use labels to create relationships between
+      resources, use the name of the composite resource as the label value.
+
+4. If there are existing composed resources:
+    a. You can update an existing composed resource by including it in your
+       output with any changes you deem necessary based on the input. Try to
+       reuse existing composed resource values as much as possible. Only
+       change values when you're sure it's necessary.
+    b. If the input indicates that a resource is no longer required, you can
+       delete it by omitting it from your output.
+
+5. Your output must only be a stream of YAML manifests, each separated by
+   "---". Submit the YAML stream to the submit_yaml_stream tool.
+</instructions>
+
+<example>
+---
+apiVersion: [api-version]
+kind: [resource-kind]
+metadata:
+  annotations:
+    upbound.io/name: [resource-kind]
+  labels:
+    [relationship-labels-if-needed]
+spec:
+  [resource-specific-fields]
+---
+[Additional resources as needed]
+</example>
+`
+
+const vars = `
+Here is the composite resource you'll be working with:
+
+<composite>
+{{ .Composite }}
+</composite>
+
+If there are any existing composed resources, they will be provided here:
+
+<composed>
+{{ .Composed }}
+</composed>
+
+Additional input is provided here:
+
+<input>
+{{ .Input }}
+</input>
+`
+
+const (
+	submitYAMLName             = "submit_yaml_stream"
+	submitYAMLSchemaProperties = `{"yaml_stream":{"type": "string","description":"The YAML stream to submit"}}`
+	submitYAMLDescription      = `
+Accepts a YAML stream to be submitted to the Kubernetes server. If this tool
+returns an error, retry the submission with a fixed version of the YAML.
+`
+)
+
+// Variables used to form the prompt.
+type Variables struct {
+	// Observed composite resource, as a YAML manifest.
+	Composite string
+
+	// Observed composed resources, as a stream of YAML manifests.
+	Composed string
+
+	// Input - i.e. user prompt.
+	Input string
+}
+
+// Function asks Claude to compose resources.
 type Function struct {
 	fnv1.UnimplementedFunctionRunnerServiceServer
 
-	log logging.Logger
+	vars *template.Template
+	log  logging.Logger
+}
+
+// NewFunction creates a new function powered by Claude.
+func NewFunction(log logging.Logger) *Function {
+	return &Function{
+		log:  log,
+		vars: template.Must(template.New("vars").Parse(vars)),
+	}
 }
 
 // RunFunction runs the Function.
-func (f *Function) RunFunction(_ context.Context, req *fnv1.RunFunctionRequest) (*fnv1.RunFunctionResponse, error) {
-	f.log.Info("Running function", "tag", req.GetMeta().GetTag())
+func (f *Function) RunFunction(ctx context.Context, req *fnv1.RunFunctionRequest) (*fnv1.RunFunctionResponse, error) { //nolint:gocyclo // TODO(negz): Factor out the API calling bits.
+	log := f.log.WithValues("tag", req.GetMeta().GetTag())
+	log.Info("Running function", "tag", req.GetMeta().GetTag())
 
 	rsp := response.To(req, response.DefaultTTL)
 
 	in := &v1alpha1.Prompt{}
 	if err := request.GetInput(req, in); err != nil {
-		// You can set a custom status condition on the claim. This allows you to
-		// communicate with the user. See the link below for status condition
-		// guidance.
-		// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
-		response.ConditionFalse(rsp, "FunctionSuccess", "InternalError").
-			WithMessage("Something went wrong.").
-			TargetCompositeAndClaim()
-
-		// You can emit an event regarding the claim. This allows you to communicate
-		// with the user. Note that events should be used sparingly and are subject
-		// to throttling; see the issue below for more information.
-		// https://github.com/crossplane/crossplane/issues/5802
-		response.Warning(rsp, errors.New("something went wrong")).
-			TargetCompositeAndClaim()
-
 		response.Fatal(rsp, errors.Wrapf(err, "cannot get Function input from %T", req))
 		return rsp, nil
 	}
 
-	// TODO: Add your Function logic here!
-	response.Normalf(rsp, "I was run with input %q!", in.Prompt)
-	f.log.Info("I was run!", "input", in.Prompt)
+	c, err := request.GetCredentials(req, credName)
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot get Anthropic API key from credential %q", credName))
+		return rsp, nil
+	}
+	if c.Type != resource.CredentialsTypeData {
+		response.Fatal(rsp, errors.Errorf("expected credential %q to be %q, got %q", credName, resource.CredentialsTypeData, c.Type))
+		return rsp, nil
+	}
+	b, ok := c.Data[credKey]
+	if !ok {
+		response.Fatal(rsp, errors.Errorf("credential %q is missing required key %q", credName, credKey))
+		return rsp, nil
+	}
 
-	// You can set a custom status condition on the claim. This allows you to
-	// communicate with the user. See the link below for status condition
-	// guidance.
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
-	response.ConditionTrue(rsp, "FunctionSuccess", "Success").
-		TargetCompositeAndClaim()
+	// TODO(negz): Where the heck is the newline at the end of this key
+	// coming from? Bug in crossplane render?
+	key := strings.Trim(string(b), "\n")
 
+	// TODO(negz): I'm using YAML as input/output because I assume the model
+	// will be better able to represent Kubernetes stuff as YAML manifests
+	// than as e.g. JSON. YAML's much more prevalent in examples etc. Could
+	// be worth validating this - could we use JSON instead to skip extra
+	// conversion?
+	xr, err := CompositeToYAML(req.GetObserved().GetComposite())
+	if err != nil {
+		response.Fatal(rsp, errors.Wrap(err, "cannot convert observed XR to YAML"))
+		return rsp, nil
+	}
+
+	cds, err := ComposedToYAML(req.GetObserved().GetResources())
+	if err != nil {
+		response.Fatal(rsp, errors.Wrap(err, "cannot convert observed composed resources to YAML"))
+		return rsp, nil
+	}
+
+	vars := &strings.Builder{}
+	if err := f.vars.Execute(vars, &Variables{Composite: xr, Composed: cds, Input: in.Prompt}); err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot build prompt from template"))
+		return rsp, nil
+	}
+
+	log.Debug("Using prompt", "prompt", vars.String())
+
+	model, err := openaillm.New(openaillm.WithToken(key))
+
+	agent := agents.NewOneShotAgent(
+		model,
+		[]tools.Tool{},
+		agents.WithMaxIterations(3),
+	)
+
+	// client := anthropic.NewClient(option.WithAPIKey(key))
+
+	_, err = chains.Run(
+		ctx,
+		agents.NewExecutor(agent),
+		chains.WithTemperature(float64(0)),
+		// strings.Join(steps, "\n"),
+	)
+
+	// messages := []anthropic.MessageParam{
+	// 	{
+	// 		Role: anthropic.MessageParamRoleUser,
+	// 		Content: []anthropic.ContentBlockParamUnion{
+	// 			{
+	// 				OfText: &anthropic.TextBlockParam{
+	// 					Text:         prompt,
+	// 					CacheControl: anthropic.NewCacheControlEphemeralParam(),
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// 	{
+	// 		Role: anthropic.MessageParamRoleUser,
+	// 		Content: []anthropic.ContentBlockParamUnion{
+	// 			{
+	// 				OfText: &anthropic.TextBlockParam{
+	// 					Text: vars.String(),
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// }
+	// for {
+	// message, err := client.Messages.New(ctx, anthropic.MessageNewParams{
+	// 	MaxTokens: 1024,
+	// 	Model:     anthropic.ModelClaudeSonnet4_0,
+	// 	System: []anthropic.TextBlockParam{
+	// 		{
+	// 			Text:         system,
+	// 			CacheControl: anthropic.NewCacheControlEphemeralParam(),
+	// 		},
+	// 	},
+	// 	Temperature: param.Opt[float64]{Value: 0}, // As little randomness as possible.
+	// 	Tools: []anthropic.ToolUnionParam{
+	// 		{
+	// 			OfTool: &anthropic.ToolParam{
+	// 				Name:        submitYAMLName,
+	// 				Description: anthropic.String(submitYAMLDescription),
+	// 				InputSchema: anthropic.ToolInputSchemaParam{
+	// 					Properties: map[string]any{
+	// 						"yaml_stream": map[string]any{
+	// 							"type":        "string",
+	// 							"description": "The YAML stream to submit",
+	// 						},
+	// 					},
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// 	Messages: messages,
+	// })
+	// if err != nil {
+	// 	response.Fatal(rsp, errors.Wrapf(err, "cannot message Claude"))
+	// 	return rsp, nil
+	// }
+
+	// Save Claude's response, to feed back to it on the next call.
+	// messages = append(messages, message.ToParam())
+
+	// toolResults := []anthropic.ContentBlockParamUnion{}
+	// for _, block := range message.Content {
+	// 	switch block.AsAny().(type) {
+
+	// 	// This could happen several times, as Claude calls the
+	// 	// tool to check whether its YAML is valid.
+	// 	case anthropic.ToolUseBlock:
+	// 		log.Debug("Got tool use block from Claude", "tool_name", block.Name, "tool_input", block.JSON.Input.Raw())
+
+	// 		switch block.Name {
+	// 		case submitYAMLName:
+	// 			y := gjson.Get(block.JSON.Input.Raw(), "yaml_stream").String()
+	// 			if y == "" {
+	// 				response.Fatal(rsp, errors.Errorf("Claude didn't provide 'yaml_stream' input property for %q tool", block.Name))
+	// 				return rsp, nil
+	// 			}
+
+	// 			result := ""
+	// 			dcds, err := ComposedFromYAML(y)
+	// 			if err != nil {
+	// 				result = err.Error()
+
+	// 				log.Debug("Submitted YAML stream", "result", result, "isError", true)
+	// 				toolResults = append(toolResults, anthropic.NewToolResultBlock(block.ID, result, true))
+
+	// 				continue
+	// 			}
+
+	// 			log.Debug("Received YAML manifests from Claude", "resourceCount", len(dcds))
+	// 			rsp.Desired.Resources = dcds
+
+	// 			return rsp, nil
+
+	// 		default:
+	// 			response.Fatal(rsp, errors.Errorf("Claude tried to use unknown tool %q", block.Name))
+	// 			return rsp, nil
+	// 		}
+
+	// 	// Despite the prompt, Claude insists on sending a text
+	// 	// message explaining what it's going to do before it
+	// 	// calls the tool. So this could be called several
+	// 	// times, and only sometimes with YAML.
+	// 	case anthropic.TextBlock:
+	// 		log.Debug("Received text block from Claude", "text", block.Text)
+	// 	}
+	// }
+
+	// Claude's done using tools.
+	// if len(toolResults) == 0 {
+	// 	break
+	// }
+
+	// Claude's not done using tools. Send the messages again, this
+	// time with the tool results.
+	// messages = append(messages, anthropic.NewUserMessage(toolResults...))
+	// }
+
+	// We should never get here.
+	response.Fatal(rsp, errors.New("Claude didn't return a YAML stream of composed resource manifests"))
 	return rsp, nil
+}
+
+// CompositeToYAML returns the XR as YAML.
+func CompositeToYAML(xr *fnv1.Resource) (string, error) {
+	j, err := protojson.Marshal(xr.GetResource())
+	if err != nil {
+		return "", errors.Wrap(err, "cannot convert XR to JSON")
+	}
+	y, err := yaml.JSONToYAML(j)
+	return string(y), errors.Wrap(err, "cannot convert XR to YAML")
+}
+
+// ComposedToYAML returns the supplied composed resources as a YAML stream. The
+// resources are annotated with their upbound.io/name annotations.
+func ComposedToYAML(cds map[string]*fnv1.Resource) (string, error) {
+	composed := &strings.Builder{}
+
+	for name, ocd := range cds {
+		jocd, err := protojson.Marshal(ocd.GetResource())
+		if err != nil {
+			return "", errors.Wrap(err, "cannot convert composed resource to JSON")
+		}
+
+		jocd, err = sjson.SetBytes(jocd, "metadata.annotations.upbound\\.io/name", name)
+		if err != nil {
+			return "", errors.Wrapf(err, "cannot set upbound.io/name annotation")
+		}
+
+		yocd, err := yaml.JSONToYAML(jocd)
+		if err != nil {
+			return "", errors.Wrap(err, "cannot convert composed resource to YAML")
+		}
+		composed.WriteString("---\n")
+		composed.Write(yocd)
+	}
+
+	return composed.String(), nil
+}
+
+// ComposedFromYAML parses the supplied YAML stream as desired composed
+// resources. The resource names are extracted from the upbound.io/name
+// annotation.
+func ComposedFromYAML(y string) (map[string]*fnv1.Resource, error) {
+	out := make(map[string]*fnv1.Resource)
+
+	for _, doc := range strings.Split(y, "---") {
+		if doc == "" {
+			continue
+		}
+		j, err := yaml.YAMLToJSON([]byte(doc))
+		if err != nil {
+			return nil, errors.Wrap(err, "cannot parse YAML")
+		}
+
+		s := &structpb.Struct{}
+		if err := protojson.Unmarshal(j, s); err != nil {
+			return nil, errors.Wrap(err, "cannot parse JSON")
+		}
+
+		name := gjson.GetBytes(j, "metadata.annotations.upbound\\.io/name").String()
+		if name == "" {
+			return nil, errors.New("missing 'upbound.io/name' annotation")
+		}
+		if _, seen := out[name]; seen {
+			return nil, errors.Errorf("'upbound.io/name' annotation %q must be unique within the YAML stream", name)
+		}
+		out[name] = &fnv1.Resource{Resource: s}
+	}
+
+	return out, nil
 }
--- go.mod
+++ go.mod
@@ -8,15 +8,26 @@ require (
 	github.com/alecthomas/kong v0.9.0
 	github.com/crossplane/function-sdk-go v0.4.0
 	github.com/google/go-cmp v0.6.0
+	github.com/tidwall/gjson v1.18.0
+	github.com/tidwall/sjson v1.2.5
+	github.com/tmc/langchaingo v0.1.13
 	google.golang.org/protobuf v1.34.3-0.20240816073751-94ecbc261689
 	k8s.io/apimachinery v0.31.0
 	sigs.k8s.io/controller-tools v0.16.0
+	sigs.k8s.io/yaml v1.4.0
 )
 
 require (
+	cloud.google.com/go/iam v1.1.10 // indirect
+	cloud.google.com/go/longrunning v0.5.9 // indirect
 	dario.cat/mergo v1.0.1 // indirect
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver/v3 v3.2.0 // indirect
+	github.com/Masterminds/sprig/v3 v3.2.3 // indirect
 	github.com/crossplane/crossplane-runtime v1.18.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/evanphx/json-patch/v5 v5.9.0 // indirect
 	github.com/fatih/color v1.17.0 // indirect
@@ -33,22 +44,39 @@ require (
 	github.com/google/gnostic-models v0.6.8 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/goph/emperror v0.17.2 // indirect
+	github.com/huandu/xstrings v1.3.3 // indirect
+	github.com/imdario/mergo v0.3.16 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/nikolalohinski/gonja v1.5.3 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.9 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkoukk/tiktoken-go v0.1.6 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
+	github.com/yargevad/filepathx v1.0.0 // indirect
+	go.starlark.net v0.0.0-20230302034142-4b1e35fe2254 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/crypto v0.39.0 // indirect
+	golang.org/x/exp v0.0.0-20240909161429-701f63a606c0 // indirect
 	golang.org/x/mod v0.25.0 // indirect
 	golang.org/x/net v0.41.0 // indirect
 	golang.org/x/oauth2 v0.22.0 // indirect
@@ -58,6 +86,7 @@ require (
 	golang.org/x/text v0.26.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.33.0 // indirect
+	google.golang.org/api v0.189.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
 	google.golang.org/grpc v1.67.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
@@ -72,5 +101,4 @@ require (
 	sigs.k8s.io/controller-runtime v0.19.0 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
-	sigs.k8s.io/yaml v1.4.0 // indirect
 )
