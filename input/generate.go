fca587fbaa94d443fd0e94c49c500ab376ff6175
tnthornton <2375126+tnthornton@users.noreply.github.com>
2025-08-05 14:42:27 -0700

    Add license header and tooling

--- input/generate.go
+++ input/generate.go
@@ -1,3 +1,19 @@
+// /*
+// Copyright 2025 The Upbound Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// */
+
 //go:build generate
 // +build generate
 
@@ -6,7 +22,10 @@
 
 // Remove existing and generate new input manifests
 //go:generate rm -rf ../package/input/
-//go:generate go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen paths=./v1alpha1 object crd:crdVersions=v1 output:artifacts:config=../package/input
+//go:generate go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen paths=./v1alpha1 object:headerFile=../hack/boilerplate.go.txt crd:crdVersions=v1 output:artifacts:config=../package/input
+
+// Add license headers to all files.
+//go:generate go tool addlicense -c "Upbound Inc" -v -f ../hack/boilerplate.go.txt . ../apis ../input ../internal
 
 package input
 
